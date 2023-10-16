70b6eb04a13fc13b968fe1b3d6ce616ae2e81f05
Angelo Gentile III <angelo.john.gentile@gmail.com>
2023-10-16 12:52:15 -0400

    first adjusted commit

--- index.js
+++ index.js
@@ -1,8 +1,38 @@
-const gameBoardArea = document.getElementById('game-board-area')
+const gameBoardArea = document.getElementById('game-board-area');
+const characterSearchInput = document.getElementById('character-id-input');
+const characterSearchForm = document.getElementById('search-bar-form');
+const characterImageSpanElement = document.getElementById('character-image');
 
-fetch('https://nekos.best/api/v2/neko?amount=20')
+let desiredCharacter;
+let allAvailableCharacters;
+
+fetch('http://localhost:3000/results')
   .then(response => response.json())
   .then(results => {
-    const returnResults = results.results;
-    console.log(returnResults)
+    allAvailableCharacters = results
+
+    console.log(allAvailableCharacters)
 });
+
+characterSearchForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    allAvailableCharacters.map(character => {
+        if (Number(character.id) === Number(characterSearchInput.value) && (document.querySelector('span#character-image img'))) {
+            let characterImage = document.getElementById('desired-character-image');
+            characterImage.src = character.url
+
+        } else if (Number(character.id) === Number(characterSearchInput.value)) {
+            const newLineBreak = document.createElement('br');
+            const newCharacterImageElement = document.createElement('img');
+            newCharacterImageElement.id = 'desired-character-image'
+            newCharacterImageElement.src = character.url;
+            characterImageSpanElement.appendChild(newLineBreak);
+            characterImageSpanElement.appendChild(newCharacterImageElement);
+            
+        } else {
+            console.log('Oh crap')
+        }
+    })
+    characterSearchForm.reset();
+})
